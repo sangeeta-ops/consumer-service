bd681b0426e5bfe03cec87b224980df87161b330
Sangeeta Khare <sangeetakhare021991@gmail.com>
2020-11-12 15:15:53 +0530

    Remove unnecessary customer table and add cloud Kafka configuration for producer.

--- src/main/java/com/prokarma/consumer/repository/KafkaDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prokarma.consumer.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.prokarma.consumer.entity.CustomerEntity;
-
-public interface KafkaDataRepository extends CrudRepository<CustomerEntity, Integer> {
-
-}
--- src/main/java/com/prokarma/consumer/service/DefaultKafkaConsumerService.java
+++ src/main/java/com/prokarma/consumer/service/DefaultKafkaConsumerService.java
@@ -9,23 +9,17 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.prokarma.consumer.entity.AddressEntity;
 import com.prokarma.consumer.entity.AuditEntity;
-import com.prokarma.consumer.entity.CustomerEntity;
 import com.prokarma.consumer.entity.ErrorEntity;
 import com.prokarma.consumer.masking.CustomerDataMasking;
 import com.prokarma.consumer.model.MessageRequest;
 import com.prokarma.consumer.repository.AuditDataRepository;
 import com.prokarma.consumer.repository.ErrorDataRepository;
-import com.prokarma.consumer.repository.KafkaDataRepository;
 
 @Service
 public class DefaultKafkaConsumerService implements kafkaConsumerService {
 
 	private static final Logger logger = LoggerFactory.getLogger(DefaultKafkaConsumerService.class);
-
-	@Autowired
-	private KafkaDataRepository kafkaDataRepository;
 
 	@Autowired
 	private AuditDataRepository auditDataRepository;
@@ -34,7 +28,7 @@ public class DefaultKafkaConsumerService implements kafkaConsumerService {
 	private ErrorDataRepository errorDataRepository;
 
 	@Override
-	@KafkaListener(topics = "first_topic")
+	@KafkaListener(topics = "${cloudkarafka.topic}")
 	public void consume(String messageRequestString) {
 		ObjectMapper objectMapper = new ObjectMapper();
 		MessageRequest messageRequest = null;
@@ -44,8 +38,6 @@ public class DefaultKafkaConsumerService implements kafkaConsumerService {
 			auditDataRepository.save(auditEntity);
 			messageRequest = CustomerDataMasking.maskCustomerData(messageRequest);
 			logger.info("Start to consume messageRequest : {} ", messageRequest);
-			CustomerEntity entity = buildCustomerEntityObject(messageRequest);
-			entity = kafkaDataRepository.save(entity);
 			logger.info("Finished to consume messageRequest : {} ", messageRequest);
 		} catch (Exception e) {
 			ErrorEntity errorEntity = buildErrorEntity(messageRequestString, e);
@@ -59,30 +51,6 @@ public class DefaultKafkaConsumerService implements kafkaConsumerService {
 		auditEntity.setCustomerNumber(messageRequest.getCustomerNumber());
 		auditEntity.setPayload(messageRequestString);
 		return auditEntity;
-	}
-
-	private CustomerEntity buildCustomerEntityObject(MessageRequest messageRequest) {
-		CustomerEntity entity = new CustomerEntity();
-		entity.setCountry(messageRequest.getCountry());
-		entity.setBirthDate(messageRequest.getBirthDate());
-		entity.setCountryCode(messageRequest.getCountryCode());
-		entity.setCustomerNumber(messageRequest.getCustomerNumber());
-		entity.setEmail(messageRequest.getEmail());
-		entity.setFirstName(messageRequest.getFirstName());
-		entity.setLastName(messageRequest.getLastName());
-		entity.setCustomerStatusEnum(messageRequest.getCustomerStatus());
-		AddressEntity addressEntity = buildAddressEntityObject(messageRequest);
-		entity.setAddress(addressEntity);
-		return entity;
-	}
-
-	private AddressEntity buildAddressEntityObject(MessageRequest messageRequest) {
-		AddressEntity addressEntity = new AddressEntity();
-		addressEntity.setAddressLine1(messageRequest.getAddress().getAddressLine1());
-		addressEntity.setAddressLine2(messageRequest.getAddress().getAddressLine2());
-		addressEntity.setPostalCode(messageRequest.getAddress().getPostalCode());
-		addressEntity.setStreet(messageRequest.getAddress().getStreet());
-		return addressEntity;
 	}
 
 	private ErrorEntity buildErrorEntity(String messageRequestString, Exception e) {

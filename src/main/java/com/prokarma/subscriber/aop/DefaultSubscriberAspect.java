98a487fff5d3ec98ff2380595e6e748680c31404
Sangeeta Khare <sangeetakhare021991@gmail.com>
2020-11-19 16:03:25 +0530

    [1221]-Code refactor.

--- src/main/java/com/prokarma/subscriber/aop/DefaultSubscriberAspect.java
+++ src/main/java/com/prokarma/subscriber/aop/DefaultSubscriberAspect.java
@@ -10,7 +10,6 @@ import com.prokarma.subscriber.entity.ErrorEntity;
 import com.prokarma.subscriber.repository.ErrorDataRepository;
 
 @Aspect
-
 @Component
 public class DefaultSubscriberAspect {
 
